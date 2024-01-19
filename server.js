550a03621e6b04778a413cd028d981aee9c98a49
pinoen <pinoen@yahoo.com.ar>
2024-01-19 11:45:17 -0300

    setup a basic server which listening on PORT=3000

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("server is working")
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
