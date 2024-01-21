d9fb9a029179389c972dba4bb060a02753e6574f
pinoen <pinoen@yahoo.com.ar>
2024-01-21 19:59:04 -0300

    add and use user router

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import cookieParser from 'cookie-parser';
 // routes
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js'
+import userRouter from './routes/userRouter.js'
 
 // middlewares
 import errorHandler from './middleware/errorHandler.js';
@@ -29,6 +30,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', authenticateUser, userRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({
