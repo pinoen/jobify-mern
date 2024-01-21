a36dcaa583fc19d9691d494f5a4abb56ca7e5036
pinoen <pinoen@yahoo.com.ar>
2024-01-21 19:27:23 -0300

    add logout route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router()
-import { register, login } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 import { validateLoginInput, validateRegisterInput } from "../middleware/validation.js";
 
 router.post('/register', validateRegisterInput, register)
 router.post('/login', validateLoginInput, login)
+router.get('/logout', logout)
 
 export default router
 
