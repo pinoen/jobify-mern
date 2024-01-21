4636d26c612192eef212688e2e7911a98354e470
pinoen <pinoen@yahoo.com.ar>
2024-01-21 20:42:42 -0300

    add middleware to validate update user inputs

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAppStats, getCurrentUser, updateUser } from "../controllers/userController.js";
+import { validateUpdateUserInput } from "../middleware/validation.js";
 const router = Router()
 
 router.get('/current-user', getCurrentUser)
 router.get('/admin/app-stats', getAppStats)
-router.patch('/update-user', updateUser)
+router.patch('/update-user', validateUpdateUserInput, updateUser)
 
 export default router
