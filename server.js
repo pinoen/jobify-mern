d9eafcc053021f1838f170851c541b9e3df6662c
pinoen <pinoen@yahoo.com.ar>
2024-01-21 18:36:40 -0300

    import the authenticate user middleware and use it in the job routes and use cookie parser library

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from 'express';
 const app = express();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 
 // routes
 import jobRouter from './routes/jobRouter.js';
@@ -12,11 +13,13 @@ import authRouter from './routes/authRouter.js'
 
 // middlewares
 import errorHandler from './middleware/errorHandler.js';
+import { authenticateUser } from './middleware/auth.js';
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(cookieParser())
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -24,7 +27,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter)
 
 app.use("*", (req, res) => {
