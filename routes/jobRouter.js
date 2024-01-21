cfc24bb0ca58267263b623304e16b3ca34e0061c
pinoen <pinoen@yahoo.com.ar>
2024-01-21 11:29:41 -0300

    add validation param middleware for routes that need it such as get job, update job and delete job

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 const router = Router();
 import { createJob, getAllJobs, getJob, updateJob, deleteJob } from "../controllers/jobControllers.js";
-import { validateJobInput } from "../middleware/validation.js";
+import { validateJobInput, validateIdParam } from "../middleware/validation.js";
 
 router.route("/").post(validateJobInput, createJob).get(getAllJobs);
-router.route("/:id").get(getJob).patch(validateJobInput, updateJob).delete(deleteJob);
+router.route("/:id").get(validateIdParam, getJob).patch(validateIdParam, validateJobInput, updateJob).delete(validateIdParam, deleteJob);
 
 export default router
