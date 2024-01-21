5f70c9a19cb6beef5f17140c0e6f02e3a9bb2ae4
pinoen <pinoen@yahoo.com.ar>
2024-01-21 15:18:40 -0300

    add logic so only the first user is admin and import hash password util

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,16 @@
 import { StatusCodes } from "http-status-codes"
 import userModel from "../models/userModel.js"
+import { hashPassword } from "../utils/passwordUtils.js"
 
 export const register = async (req, res) => {
+  const isFirstAccount = (await userModel.countDocuments()) === 0
+  req.body.role = isFirstAccount ? 'admin' : 'user'
+
+  const hashedPassword = await hashPassword(req.body.password)
+  req.body.password = hashedPassword
+
   const user = await userModel.create(req.body)
-  res.status(StatusCodes.CREATED).json({ user })
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' })
 }
 
 export const login = async (req, res) => {
