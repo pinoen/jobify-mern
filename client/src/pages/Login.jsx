945582a1c02735a08d0af29ed25e4169fea13c35
pinoen <pinoen@yahoo.com.ar>
2024-01-22 15:33:21 -0300

    add action fn to manage the login request, and replace the html form by the react router dom one

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,19 +1,38 @@
-import { Link } from "react-router-dom"
+import { Form, Link, redirect, useNavigation } from "react-router-dom"
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage"
 import { FormRow, Logo } from "../components"
+import customFetch from "../utils/customFetch"
+import { toast } from "react-toastify"
+
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+
+  try {
+    await customFetch.post('/auth/login', data)
+    toast.success('Login successful')
+    return redirect('/dashboard')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
 
 const Login = () => {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Login</h4>
 
         <FormRow type="email" name="email" defaultValue="emiliopino@me.com" />
         <FormRow type="password" name="password" defaultValue="123456" />
 
-        <button type="submit" className="btn btn-block">
-          Submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...' : 'Submit'}
         </button>
         <button type="button" className="btn btn-block">
           Explore Demo
@@ -21,7 +40,7 @@ const Login = () => {
         <p>
           Do not have an account? <Link to="/register" className="member-btn">Register</Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   )
 }
