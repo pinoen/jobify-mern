9fecffd7bc302262bb36e00ef513578002a9142c
pinoen <pinoen@yahoo.com.ar>
2024-01-22 19:50:54 -0300

    create action to create a new job

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,23 @@
-import { Form, useNavigation, useOutletContext } from "react-router-dom"
+import { Form, redirect, useNavigation, useOutletContext } from "react-router-dom"
 import Wrapper from "../assets/wrappers/DashboardFormPage"
 import { FormRow, FormRowSelect } from "../components"
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+
+  try {
+    await customFetch.post('/jobs', data)
+    toast.success('Job created successfully')
+    return redirect('/dashboard/all-jobs')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
 
 const AddJob = () => {
   const { user } = useOutletContext()
