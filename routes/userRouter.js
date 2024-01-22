8b79d2cd8172407b51bdf5a7c8b395efce0f93c7
pinoen <pinoen@yahoo.com.ar>
2024-01-21 23:27:52 -0300

    add authorizePermissions middleware for the get stats route and pass it as argument the admin role

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getAppStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { validateUpdateUserInput } from "../middleware/validation.js";
+import { authorizePermissions } from "../middleware/auth.js";
 const router = Router()
 
 router.get('/current-user', getCurrentUser)
-router.get('/admin/app-stats', getAppStats)
+router.get('/admin/app-stats', authorizePermissions('admin'), getAppStats)
 router.patch('/update-user', validateUpdateUserInput, updateUser)
 
 export default router
