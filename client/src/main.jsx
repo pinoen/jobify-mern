bcb93f8dd941bfb9ed69e4a4e1f10d73d445a923
pinoen <pinoen@yahoo.com.ar>
2024-01-22 00:19:38 -0300

    same test but using axios library

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios'
 
-fetch('/api/v1/test').then(res => res.json()).then(data => console.log(data))
+const response = await axios.get('/api/v1/test')
+console.log(response)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
