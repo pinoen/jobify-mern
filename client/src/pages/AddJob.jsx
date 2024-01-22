b507150e98ce3134a989d358c3c02662f352eeaf
pinoen <pinoen@yahoo.com.ar>
2024-01-22 19:02:01 -0300

    add input into the add job form

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,29 @@
+import { Form, useNavigation, useOutletContext } from "react-router-dom"
+import Wrapper from "../assets/wrappers/DashboardFormPage"
+import { FormRow, FormRowSelect } from "../components"
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
 
 const AddJob = () => {
+  const { user } = useOutletContext()
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+
   return (
-    <div>AddJob</div>
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">add job</h4>
+        <div className="form-center">
+          <FormRow type='text' name='position' />
+          <FormRow type='text' name='company' />
+          <FormRow type='text' name='location' defaultValue={user.location} />
+          <FormRowSelect name='status' list={Object.values(JOB_STATUS)} defaultValue={JOB_STATUS.PENDING} />
+          <FormRowSelect name='type' list={(Object.values(JOB_TYPE))} defaultValue={JOB_TYPE.FULL_TIME} />
+          <button type="submit" className="btn btn-block form-btn" disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting' : 'Submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
   )
 }
 
