371b77fb88d64fd78d101d40d663ce7dc47a137c
pinoen <pinoen@yahoo.com.ar>
2024-01-23 12:59:40 -0300

    add loader to get app stats

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,7 +1,26 @@
+import { toast } from "react-toastify"
+import customFetch from "../utils/customFetch"
+import { redirect, useLoaderData } from "react-router-dom"
+import Wrapper from "../assets/wrappers/StatsContainer"
 
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/users/admin/app-stats')
+    return response.data
+  } catch (error) {
+    toast.error('You are not authorized to view this page')
+    return redirect('/dashboard')
+  }
+}
 const Admin = () => {
+  const { users, jobs } = useLoaderData()
+  console.log(users, jobs)
+
   return (
-    <div>Admin</div>
+    <Wrapper>
+      <h1>admin page</h1>
+    </Wrapper>
   )
 }
 
