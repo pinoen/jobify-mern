f8b97993df85851c89303a7d55ce45854cdf3815
pinoen <pinoen@yahoo.com.ar>
2024-01-23 13:00:39 -0300

    add fn to refactor link so in case the user is not an admin cannot see the admin page

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -4,11 +4,13 @@ import { links } from "../utils/links"
 import { useDashboardContext } from "../pages/DashboardLayout"
 
 const NavLinks = ({ isBigSidebar }) => {
-  const { toggleSidebar } = useDashboardContext()
+  const { toggleSidebar, user } = useDashboardContext()
+
+  let refactorLinks = user.role === 'admin' ? links : links.filter(link => link.text !== 'admin')
 
   return (
     <div className='nav-links'>
-      {links.map((link) => {
+      {refactorLinks.map((link) => {
         const { path, text, icon } = link
         return (
           <NavLink
