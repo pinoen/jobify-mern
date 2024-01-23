a92d161b106dfcba482d143a3761072816bf7356
pinoen <pinoen@yahoo.com.ar>
2024-01-23 12:01:44 -0300

    create edit job component with loader and action fns

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,7 +1,56 @@
+import { toast } from "react-toastify"
+import customFetch from "../utils/customFetch"
+import { Form, redirect, useLoaderData, useNavigation } from "react-router-dom"
+import Wrapper from "../assets/wrappers/DashboardFormPage"
+import { FormRow, FormRowSelect } from "../components"
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants"
 
+
+export const loader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`)
+    return data
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return redirect('../all-jobs')
+  }
+}
+
+export const action = async ({ request, params }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+
+  try {
+    await customFetch.patch(`/jobs/${params.id}`, data)
+    toast.success('Job updated successfully')
+    return redirect('../all-jobs')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
 const EditJob = () => {
+  const { foundJob } = useLoaderData()
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === "submitting"
+
   return (
-    <div>EditJob</div>
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">edit job</h4>
+        <div className="form-center">
+          <FormRow type='text' name='position' defaultValue={foundJob.position} />
+          <FormRow type='text' name='company' defaultValue={foundJob.company} />
+          <FormRow type='text' name='location' defaultValue={foundJob.location} />
+          <FormRowSelect name='status' defaultValue={foundJob.status} list={Object.values(JOB_STATUS)} />
+          <FormRowSelect name='type' defaultValue={foundJob.type} list={Object.values(JOB_TYPE)} />
+
+          <button type="submit" className="btn btn-block form-btn" disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
   )
 }
 
