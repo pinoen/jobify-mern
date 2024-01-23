6c8bc4eddadc30e945b98186ce24d214714107ab
pinoen <pinoen@yahoo.com.ar>
2024-01-23 11:02:14 -0300

    render jobs taking from the context inside the job container

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,7 +1,25 @@
+import Wrapper from "../assets/wrappers/JobsContainer"
+import { useAllJobsContext } from "../pages/AllJobs"
+import Job from "./Job"
 
 const JobsContainer = () => {
+  const { data } = useAllJobsContext()
+  const { jobs } = data
+
+  if (jobs.length === 0) {
+    return <Wrapper>
+      <h2>No jobs to display...</h2>
+    </Wrapper>
+  }
+
   return (
-    <h2>JobsContainer</h2>
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />
+        })}
+      </div>
+    </Wrapper>
   )
 }
 
