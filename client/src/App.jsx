2e677ebe33cbd9a6ecd85c6d6dba21fdfc4f5e76
pinoen <pinoen@yahoo.com.ar>
2024-01-23 17:05:13 -0300

    add action fn for the profile route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import { loader as editJobLoader } from "./pages/EditJob"
 import { action as editJobAction } from "./pages/EditJob"
 import { action as deleteAction } from "./pages/DeleteJob"
 import { loader as adminLoader } from "./pages/Admin"
+import { action as profileAction } from "./pages/Profile"
 
 export const checkDefaultTheme = () => {
   const isDarkMode = localStorage.getItem('isDarkMode') === 'true'
@@ -69,7 +70,8 @@ const router = createBrowserRouter([
           },
           {
             path: "profile",
-            element: <Profile />
+            element: <Profile />,
+            action: profileAction
           },
           {
             path: "admin",
