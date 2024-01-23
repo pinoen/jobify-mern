2e5d605aafd8abc2a61db6731047afd795f99f27
pinoen <pinoen@yahoo.com.ar>
2024-01-23 11:08:53 -0300

    add route for the link when editing

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -30,7 +30,7 @@ const Job = ({ _id, position, company, location, type, createdAt, status }) => {
           </div>
         </div>
         <footer className="actions">
-          <Link className="btn edit-btn">Edit</Link>
+          <Link to={`../edit-job/${_id}`} className="btn edit-btn">Edit</Link>
           <Form>
             <button type="submit" className="btn delete-btn">
               Delete
