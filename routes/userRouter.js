48a24e3457bc6c950db791d308cf3953e739406a
pinoen <pinoen@yahoo.com.ar>
2024-01-23 17:06:26 -0300

    add multer middleware in the update user route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { getAppStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { validateUpdateUserInput } from "../middleware/validation.js";
 import { authorizePermissions } from "../middleware/auth.js";
+import upload from "../middleware/multer.js";
 const router = Router()
 
 router.get('/current-user', getCurrentUser)
 router.get('/admin/app-stats', authorizePermissions('admin'), getAppStats)
-router.patch('/update-user', validateUpdateUserInput, updateUser)
+router.patch('/update-user', upload.single('avatar'), validateUpdateUserInput, updateUser)
 
 export default router
