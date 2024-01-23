6d19d982fc6e7414cb3a2260546cc0f5f843fce6
pinoen <pinoen@yahoo.com.ar>
2024-01-23 13:18:16 -0300

    add stat item as content for the admin page

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -2,6 +2,8 @@ import { toast } from "react-toastify"
 import customFetch from "../utils/customFetch"
 import { redirect, useLoaderData } from "react-router-dom"
 import Wrapper from "../assets/wrappers/StatsContainer"
+import { StatItem } from "../components"
+import { FaCalendarCheck, FaSuitcaseRolling } from "react-icons/fa"
 
 
 export const loader = async () => {
@@ -19,7 +21,8 @@ const Admin = () => {
 
   return (
     <Wrapper>
-      <h1>admin page</h1>
+      <StatItem title='current users' count={users} color='#e9b949' bg='#fcefc7' icon={<FaSuitcaseRolling />} />
+      <StatItem title='total jobs' count={jobs} color='#647acb' bg='#e0e8f9' icon={<FaCalendarCheck />} />
     </Wrapper>
   )
 }
