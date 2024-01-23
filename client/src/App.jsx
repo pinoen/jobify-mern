d7a3a41c94c4534d933467315ca4fcf44a96fc5a
pinoen <pinoen@yahoo.com.ar>
2024-01-23 12:01:10 -0300

    add loader and action fns for edit job route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import { action as loginAction } from "./pages/Login"
 import { loader as dashboardLoader } from "./pages/DashboardLayout"
 import { action as addJobAction } from "./pages/AddJob"
 import { loader as allJobsLoader } from "./pages/AllJobs"
+import { loader as editJobLoader } from "./pages/EditJob"
+import { action as editJobAction } from "./pages/EditJob"
 
 export const checkDefaultTheme = () => {
   const isDarkMode = localStorage.getItem('isDarkMode') === 'true'
@@ -46,7 +48,9 @@ const router = createBrowserRouter([
           },
           {
             path: 'edit-job/:id',
-            element: <EditJob />
+            element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction
           },
           {
             path: "all-jobs",
