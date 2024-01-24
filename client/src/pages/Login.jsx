1fb8b0ae0f6cceeb36c1aa0fd2da932053342524
pinoen <pinoen@yahoo.com.ar>
2024-01-23 23:18:39 -0300

    add fn for login a demo user for demonstration purposes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Form, Link, redirect } from "react-router-dom"
+import { Form, Link, redirect, useNavigate } from "react-router-dom"
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage"
 import { FormRow, Logo, SubmitBtn } from "../components"
 import customFetch from "../utils/customFetch"
@@ -19,6 +19,22 @@ export const action = async ({ request }) => {
 }
 
 const Login = () => {
+  const navigate = useNavigate()
+  const loginDemoUser = async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'secret123'
+    }
+
+    try {
+      await customFetch.post('/auth/login', data)
+      toast.success('Enjoy the demo')
+      navigate('/dashboard')
+    } catch (error) {
+      toast.error(error?.response?.data?.msg)
+    }
+
+  }
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -27,7 +43,7 @@ const Login = () => {
         <FormRow type="email" name="email" defaultValue="emiliopino@me.com" />
         <FormRow type="password" name="password" defaultValue="emilio123" />
         <SubmitBtn />
-        <button type="button" className="btn btn-block">
+        <button type="button" className="btn btn-block" onClick={loginDemoUser}>
           Explore Demo
         </button>
         <p>
