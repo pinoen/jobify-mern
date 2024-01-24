218dc3650f851c38b66b8b7d9de0bc95aca7d5dc
pinoen <pinoen@yahoo.com.ar>
2024-01-24 11:17:47 -0300

    add checkForTestUser middleware to prevent updating profiles

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { getAppStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { validateUpdateUserInput } from "../middleware/validation.js";
-import { authorizePermissions } from "../middleware/auth.js";
+import { authorizePermissions, checkForTestUser } from "../middleware/auth.js";
 import upload from "../middleware/multer.js";
 const router = Router()
 
 router.get('/current-user', getCurrentUser)
 router.get('/admin/app-stats', authorizePermissions('admin'), getAppStats)
-router.patch('/update-user', upload.single('avatar'), validateUpdateUserInput, updateUser)
+router.patch('/update-user', checkForTestUser, upload.single('avatar'), validateUpdateUserInput, updateUser)
 
 export default router
