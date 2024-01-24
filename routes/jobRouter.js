2c29d9fdcfbc0eec1346af610d8d441039e3db0a
pinoen <pinoen@yahoo.com.ar>
2024-01-24 12:17:16 -0300

    add a stats route and add it a showStats controller

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
-import { createJob, getAllJobs, getJob, updateJob, deleteJob } from "../controllers/jobControllers.js";
+import { createJob, getAllJobs, getJob, updateJob, deleteJob, showStats } from "../controllers/jobControllers.js";
 import { validateJobInput, validateIdParam } from "../middleware/validation.js";
 import { checkForTestUser } from "../middleware/auth.js";
 
 router.route("/").post(checkForTestUser, validateJobInput, createJob).get(getAllJobs);
+router.route('/stats').get(showStats)
 router.route("/:id").get(validateIdParam, getJob).patch(checkForTestUser, validateIdParam, validateJobInput, updateJob).delete(checkForTestUser, validateIdParam, deleteJob);
 
 export default router
