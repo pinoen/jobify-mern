17189047eeae516bbb05657c45992179c181c06d
pinoen <pinoen@yahoo.com.ar>
2024-01-24 17:05:48 -0300

    add structure to component

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,7 +1,43 @@
+import { Form, Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/DashboardFormPage"
+import FormRow from "./FormRow"
+import FormRowSelect from "./FormRowSelect"
+import { JOB_SORT_BY, JOB_STATUS, JOB_TYPE } from "../../../utils/constants"
+import SubmitBtn from "./SubmitBtn"
 
 const SearchContainer = () => {
   return (
-    <h2>SearchContainer</h2>
+    <Wrapper>
+      <Form className='form'>
+        <h5 className='form-title'>search form</h5>
+        <div className='form-center'>
+          {/* search position */}
+
+          <FormRow type='search' name='search' labelText='Search query' defaultValue='a' />
+          <FormRowSelect
+            name='status'
+            list={['all', ...Object.values(JOB_STATUS)]}
+            defaultValue='all'
+          />
+          <FormRowSelect
+            name='type'
+            list={['all', ...Object.values(JOB_TYPE)]}
+            defaultValue='all'
+          />
+          <FormRowSelect
+            name='sort'
+            defaultValue='newest'
+            list={[...Object.values(JOB_SORT_BY)]}
+          />
+
+          <Link to='/dashboard/all-jobs' className='btn form-btn delete-btn'>
+            Reset Search Values
+          </Link>
+          {/* TEMP!!!! */}
+          <SubmitBtn formBtn />
+        </div>
+      </Form>
+    </Wrapper>
   )
 }
 
