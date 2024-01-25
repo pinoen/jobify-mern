565183e9c2bce96951da4f7b8a5320753f0fcb5f
pinoen <pinoen@yahoo.com.ar>
2024-01-25 13:17:05 -0300

    use onChange pass by prop and use useSubmit hook so to updated query automatically when some input changes

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,40 +1,46 @@
-import { Form, Link } from "react-router-dom"
+import { Form, Link, useSubmit } from "react-router-dom"
 import Wrapper from "../assets/wrappers/DashboardFormPage"
 import FormRow from "./FormRow"
 import FormRowSelect from "./FormRowSelect"
 import { JOB_SORT_BY, JOB_STATUS, JOB_TYPE } from "../../../utils/constants"
 import SubmitBtn from "./SubmitBtn"
+import { useAllJobsContext } from "../pages/AllJobs"
 
 const SearchContainer = () => {
+  const { searchValues } = useAllJobsContext()
+  const { search, status, type, sort } = searchValues
+  const submit = useSubmit()
+
   return (
     <Wrapper>
       <Form className='form'>
         <h5 className='form-title'>search form</h5>
         <div className='form-center'>
-          {/* search position */}
-
-          <FormRow type='search' name='search' labelText='Search query' defaultValue='a' />
+          <FormRow type='search' name='search' labelText='Search query' defaultValue={search} onChange={(e) => submit(e.currentTarget.form)} />
           <FormRowSelect
             name='status'
             list={['all', ...Object.values(JOB_STATUS)]}
-            defaultValue='all'
+            defaultValue={status}
+            onChange={(e) => submit(e.currentTarget.form)}
           />
           <FormRowSelect
             name='type'
             list={['all', ...Object.values(JOB_TYPE)]}
-            defaultValue='all'
+            defaultValue={type}
+            onChange={(e) => submit(e.currentTarget.form)}
           />
           <FormRowSelect
             name='sort'
-            defaultValue='newest'
+            defaultValue={sort}
             list={[...Object.values(JOB_SORT_BY)]}
+            onChange={(e) => submit(e.currentTarget.form)}
           />
 
           <Link to='/dashboard/all-jobs' className='btn form-btn delete-btn'>
             Reset Search Values
           </Link>
           {/* TEMP!!!! */}
-          <SubmitBtn formBtn />
+          {/* <SubmitBtn formBtn /> */}
         </div>
       </Form>
     </Wrapper>
