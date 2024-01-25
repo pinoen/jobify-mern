b176b774da43f47aa6f6aa69845b4e809dd59647
pinoen <pinoen@yahoo.com.ar>
2024-01-25 17:14:50 -0300

    point to index.html

--- server.js
+++ server.js
@@ -54,6 +54,10 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', authenticateUser, userRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'))
+})
+
 app.use("*", (req, res) => {
   res.status(404).json({
     status: 'fail',
