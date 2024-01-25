70af49b7984877961c875e2c19ac5e75aab27692
pinoen <pinoen@yahoo.com.ar>
2024-01-25 13:18:48 -0300

    add params in the searchValues variables in the object returning alongside data

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -12,7 +12,7 @@ export const loader = async ({ request }) => {
 
   try {
     const { data } = await customFetch.get('/jobs', { params })
-    return { data }
+    return { data, searchValues: { ...params } }
   } catch (error) {
     toast.error(error?.response?.data?.msg)
     return error
@@ -22,10 +22,10 @@ export const loader = async ({ request }) => {
 const AllJobsContext = createContext()
 
 const AllJobs = () => {
-  const { data } = useLoaderData()
+  const { data, searchValues } = useLoaderData()
 
   return (
-    <AllJobsContext.Provider value={{ data }}>
+    <AllJobsContext.Provider value={{ data, searchValues }}>
       <SearchContainer />
       <JobsContainer />
     </AllJobsContext.Provider>
